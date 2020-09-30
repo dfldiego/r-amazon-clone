1cefb9746ff65b4d16a311ce3e4aef0f7a757692
dfldiego <dfl.diego92@gmail.com>
2020-09-30 11:33:03 -0300

    Home. parte1. creando los archivos home y de estilo del home.

---
+++ src/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src={require("./assets/img/fondo__amazon.png")} alt="imagenFondo" />
+
+                <div className="home_-row">
+                    {/* Producto */}
+                    {/* Producto */}
+                </div>
+
+                <div className="home_-row">
+                    {/* Producto */}
+                    {/* Producto */}
+                    {/* Producto */}
+                </div>
+
+                <div className="home_-row">
+                    {/* Producto */}
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
